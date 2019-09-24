9ba8c389fb6840ae2c3f10cedf6dbcbe4675d81f
Luca.CODURI <luca.coduri@cpnv.ch>
2019-09-24 09:24:48 +0200

    Ajout des tourelles(correction bug)

--- scripts/turret.js
+++ scripts/turret.js
@@ -10,15 +10,16 @@ for (var i = 0; i < turretsPositions.length; i++) {
 
 class Turret {
     constructor(level, positionX, positionY) {
-        this.level = level;
+        this.level = new Image();
+        this.level.src = level;
         this.position = {X: positionX, Y: positionY};
-        this.turretBase= new Image();
+        this.turretBase = new Image();
         this.turretBase.src = "assets/sprites/towerDefense_tile181.png";
     }
 
-    drawTurret(){
-        ctx.drawImage(this.turretBase,100 * this.position.X, 100 * this.position.Y, spritesGroundSize, spritesGroundSize);
-        ctx.drawImage(this.level,100 * this.position.X, 100 * this.position.Y, spritesGroundSize, spritesGroundSize);
+    drawTurret() {
+        ctx.drawImage(this.turretBase, spritesGroundSize * this.position.X, spritesGroundSize * this.position.Y, spritesGroundSize, spritesGroundSize);
+        ctx.drawImage(this.level, spritesGroundSize * this.position.X, spritesGroundSize * this.position.Y, spritesGroundSize, spritesGroundSize);
     }
 
     shoot() {
