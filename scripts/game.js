92ab4002ef098ed39930bc325f10e150b224187a
Luca Coduri <luca.coduri@cpnv.ch>
2019-09-28 02:23:25 +0200

    Tentative de création d'une class Game

--- scripts/game.js
+++ scripts/game.js
@@ -5,74 +5,102 @@
 //https://gamedevelopment.tutsplus.com/tutorials/collision-detection-using-the-separating-axis-theorem--gamedev-169
 //https://www.briankoponen.com/html5-javascript-game-tutorial-space-invaders-part-5/
 //cette fonction dessine sur le canevas
-function draw(secondsPassed) {
-    map.drawMap();
-    defense.drawTurret(ennemies);
-    defense1.drawTurret(ennemies);
 
-    bullet.drawBullet();
+class Game{
+    static map = new Map();
+    static defense = new Turret(levels.level1, 9, 2);
+    static defense1 = new Turret(levels.level1, 5, 2);
+    static ennemies = ennemyFactory(20,0 * spritesGroundSize,1 * spritesGroundSize,1000);
+    static bullet = new Bullet(79, 2, 600,300);
+    static turretsPositions = new Array(Game.map.cords.length);
 
-    for (var i = 0; i < ennemies.length; i++) {
-        if (ennemies[i]!== undefined) ennemies[i].drawEnnemy();
+    constructor(){
+        for (var i = 0; i < Game.turretsPositions.length; i++) {
+            Game.turretsPositions[i] = new Array(Game.map.cords[0].length);
+        }
     }
 
-    drawFPS(secondsPassed);
-}
+    start(){
+        requestAnimationFrame(this.loop);
+    }
 
-//Cette fonction met à jour les éléments
-function update(timestamp) {
-    if (key_left) {
-        ennemies[0].position.X -= 10;
-    }
-    if (key_right) {
-        ennemies[0].position.X += 10;
-    }
-    if (key_up) {
-        ennemies[0].position.Y -= 10;
-    }
-    if (key_down) {
-        ennemies[0].position.Y += 10;
-    }
-    for (var i = 0; i < ennemies.length; i++) {
-        if (ennemies[i]!== undefined) {
-            ennemies[i].updateHitbox();
-            ennemies[i].checkColisionEnnemyWaypoint();
-            ennemies[i].followWaypoints();
+    static draw(secondsPassed) {
+        Game.map.drawMap();
+        Game.defense.drawTurret(Game.ennemies);
+        Game.defense1.drawTurret(Game.ennemies);
 
+        Game.bullet.drawBullet();
+
+        for (var i = 0; i < Game.ennemies.length; i++) {
+            if (Game.ennemies[i]!== undefined) Game.ennemies[i].drawEnnemy();
         }
 
-        //ennemies[0].checkColisionEnnemyWaypoint();
+        this.drawFPS(secondsPassed);
     }
-    try {
-        //console.log(ennemies[0].position);
-        //console.log(Math.floor(ennemies[0].position.X), Math.floor(ennemies[0].position.Y));
-        //console.log("waypoint: " + ennemies[0].waypointId);
-    } catch (e) {
+
+//Cette fonction met à jour les éléments
+    static update() {
+        if (key_left) {
+            Game.ennemies[0].position.X -= 10;
+        }
+        if (key_right) {
+            Game.ennemies[0].position.X += 10;
+        }
+        if (key_up) {
+            Game.ennemies[0].position.Y -= 10;
+        }
+        if (key_down) {
+            Game.ennemies[0].position.Y += 10;
+        }
+        for (var i = 0; i < Game.ennemies.length; i++) {
+            if (Game.ennemies[i]!== undefined) {
+                Game.ennemies[i].updateHitbox();
+                Game.ennemies[i].checkColisionEnnemyWaypoint();
+                Game.ennemies[i].followWaypoints();
+
+            }
+
+            //ennemies[0].checkColisionEnnemyWaypoint();
+        }
+        try {
+            //console.log(ennemies[0].position);
+            //console.log(Math.floor(ennemies[0].position.X), Math.floor(ennemies[0].position.Y));
+            //console.log("waypoint: " + ennemies[0].waypointId);
+        } catch (e) {
+
+        }
+        Game.bullet.move();
 
     }
-    bullet.move();
-
-}
 
 //cette fonction représente la gameloop du jeu
-function loop(timestamp) {
-    //Calculate the number of seconds passed
-    //since the last frame
-    var secondsPassed = (timestamp - lastRender) / 1000;
-    //Calculate fps
+    static loop(timestamp) {
+        //Calculate the number of seconds passed
+        //since the last frame
+        var secondsPassed = (timestamp - lastRender) / 1000;
+        //Calculate fps
 
 
-    update(timestamp);
-    draw(secondsPassed);
+        this.update();
+        this.draw(secondsPassed);
 
-    lastRender = timestamp;
-    requestAnimationFrame(loop);
+        lastRender = timestamp;
+        requestAnimationFrame(this.loop);
+    }
+
+    static drawFPS(secondsPassed) {
+        var fps = Math.round(1 / secondsPassed);
+        //Draw number to the screen
+        ctx.font = '25px Arial';
+        ctx.fillStyle = 'black';
+        ctx.fillText("FPS: " + fps, 10, 30);
+    }
+
 }
 
-function drawFPS(secondsPassed) {
-    var fps = Math.round(1 / secondsPassed);
-    //Draw number to the screen
-    ctx.font = '25px Arial';
-    ctx.fillStyle = 'black';
-    ctx.fillText("FPS: " + fps, 10, 30);
-}
+
+
+
+
+
+
--- scripts/init.js
+++ scripts/init.js
@@ -2,21 +2,13 @@ let canvas;
 let ctx;
 let sprites;
 var lastRender = 0;
-var defense = new Turret(levels.level1, 9, 2);
-var defense1 = new Turret(levels.level1, 5, 2);
-var bullet = new Bullet(79, 2, 600,300);
-var map = new Map();
-
-var ennemies = ennemyFactory(20,0 * spritesGroundSize,1 * spritesGroundSize,1000);
+var game = new Game();
 
 
 
-var turretsPositions = new Array(map.cords.length);
 
 
-for (var i = 0; i < turretsPositions.length; i++) {
-    turretsPositions[i] = new Array(map.cords[0].length);
-}
+
 
 
 
@@ -36,6 +28,6 @@ function init() {
     loadSprites();
     console.log("sprites loaded");
 
-    requestAnimationFrame(loop);
+    game.start();
 
 }
