adf8551f3adcf417e81f8a3c44afb49697750822
Luca.CODURI <coduri.vitor@gmail.com>
2019-11-05 12:07:09 +0100

    Ajout de la class Base

--- scripts/base.js
+++ scripts/base.js
@@ -1,5 +1,21 @@
 class Base {
-    constructor(){
+    _spriteLocation;
+    _maxLifePoint;
+    _currentLife;
+
+    constructor(life){
+        this._maxLifePoint = life;
+        this._currentLife = this._maxLifePoint;
+    }
+
+    update(){
 
     }
+
+    draw(){
+
+    }
+
+
+
 }
